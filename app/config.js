f18fc37e1c7041e06bd189749eca168dbe9c7b9e
valgaze <valgaze@gmail.com>
2015-09-14 17:04:20 -0700

    Added user model and table

--- app/config.js
+++ app/config.js
@@ -41,6 +41,22 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
+
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255);
+      user.string('password', 255);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
+
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
