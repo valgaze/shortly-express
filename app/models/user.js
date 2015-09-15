718810d5bf748b7b207ae9664965f784f2bee79a
valgaze <valgaze@gmail.com>
2015-09-14 17:04:35 -0700

    Implement user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,12 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
-
-
+var Click = require('./click');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
 });
 
 module.exports = User;
