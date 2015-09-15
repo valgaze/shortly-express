543cd73a9f5f239ea6c6bc86fb4a1a57ddcf97ef
valgaze <valgaze@gmail.com>
2015-09-15 10:51:17 -0700

    Implement sessions-- login/logout

--- shortly.js
+++ shortly.js
@@ -12,6 +12,7 @@ var Link = require('./app/models/link');
 var Click = require('./app/models/click');
 var User = require('./app/models/user');
 var Users = require('./app/collections/users');
+var session = require('express-session');
 
 var app = express();
 app.set('views', __dirname + '/views');
@@ -23,11 +24,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
 
-app.get('/', 
+/** AUTH Implementation **/
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+function authenticate(username, password, fn) {
+  new User({username: username}).fetch().then(function(found) {
+    if (found) {
+      console.log("Terrific that user exists with this info:", found.attributes);
+        if (password === found.attributes.password){
+          fn(found);
+        }else{
+          res.send(200, "Sorry the passwords don't match");
+        }
+    } else {
+      res.send(200,"Sorry, that is not a valid username");
+    }
+  });
+}
+
+app.get('/', restrict,
 function(req, res) {
   res.render('index');
 });
+
 
 
 app.get('/login', 
@@ -40,9 +72,17 @@ function(req, res) {
   res.render('signup');
 });
 
-app.get('/create', 
+app.get('/create', restrict,  
 function(req, res) {
   res.render('index');
+});
+
+app.get('/logout', function(req, res){
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
 });
 
 app.get('/links', 
@@ -52,8 +92,13 @@ function(req, res) {
   });
 });
 
+app.get('/restricted', restrict, function(req, res){
+  res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
+});
+
+
 /**TODO: Remove this catastrophically insecure helper**/
-app.get('/users', 
+app.get('/users', restrict,
 function(req, res) {
   Users.reset().fetch().then(function(links) {
     res.send(200, links.models);
@@ -63,23 +108,27 @@ function(req, res) {
 app.post('/login',
   function(req,res){
     //Todo: Hash password check
-    var username = req.body.username;
-    var password = req.body.password;
-
-  new User({username: username}).fetch().then(function(found) {
-
-    if (found) {
-      console.log("Terrific that user exists with this info:", found.attributes);
-      if (password === found.attributes.password){
-        res.redirect("/");
-      }else{
-        res.send(200, "Sorry the passwords don't match");
-      }
+authenticate(req.body.username, req.body.password, function(user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function(){
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('/');
+      });
     } else {
-      res.send(200,"Sorry, that is not a valid username");
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('/login');
     }
   });
-
 
 });
 
