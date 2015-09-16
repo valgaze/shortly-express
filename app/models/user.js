9ae073f858e947bc1cbb3cf3fb81103b019daeae
valgaze <valgaze@gmail.com>
2015-09-15 18:52:08 -0700

    Link links to users

--- app/models/user.js
+++ app/models/user.js
@@ -2,11 +2,14 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var Click = require('./click');
+var Link = require('./link');
 
 var User = db.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
-
+  links: function(){
+    return this.hasMany(Link);
+  }
 });
 
 module.exports = User;
